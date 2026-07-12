b084bf0129f720dcac930e8b53f6e51d598ae072
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:53 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "monitor_alert_processing_rule_suppressions" {
-  description = "All monitor_alert_processing_rule_suppression resources"
-  value       = azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions
-}
 output "monitor_alert_processing_rule_suppressions_condition" {
-  description = "List of condition values across all monitor_alert_processing_rule_suppressions"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : v.condition]
+  description = "Map of condition values across all monitor_alert_processing_rule_suppressions, keyed the same as var.monitor_alert_processing_rule_suppressions"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : k => v.condition }
 }
 output "monitor_alert_processing_rule_suppressions_description" {
-  description = "List of description values across all monitor_alert_processing_rule_suppressions"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : v.description]
+  description = "Map of description values across all monitor_alert_processing_rule_suppressions, keyed the same as var.monitor_alert_processing_rule_suppressions"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : k => v.description }
 }
 output "monitor_alert_processing_rule_suppressions_enabled" {
-  description = "List of enabled values across all monitor_alert_processing_rule_suppressions"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : v.enabled]
+  description = "Map of enabled values across all monitor_alert_processing_rule_suppressions, keyed the same as var.monitor_alert_processing_rule_suppressions"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : k => v.enabled }
 }
 output "monitor_alert_processing_rule_suppressions_name" {
-  description = "List of name values across all monitor_alert_processing_rule_suppressions"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : v.name]
+  description = "Map of name values across all monitor_alert_processing_rule_suppressions, keyed the same as var.monitor_alert_processing_rule_suppressions"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : k => v.name }
 }
 output "monitor_alert_processing_rule_suppressions_resource_group_name" {
-  description = "List of resource_group_name values across all monitor_alert_processing_rule_suppressions"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : v.resource_group_name]
+  description = "Map of resource_group_name values across all monitor_alert_processing_rule_suppressions, keyed the same as var.monitor_alert_processing_rule_suppressions"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : k => v.resource_group_name }
 }
 output "monitor_alert_processing_rule_suppressions_schedule" {
-  description = "List of schedule values across all monitor_alert_processing_rule_suppressions"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : v.schedule]
+  description = "Map of schedule values across all monitor_alert_processing_rule_suppressions, keyed the same as var.monitor_alert_processing_rule_suppressions"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : k => v.schedule }
 }
 output "monitor_alert_processing_rule_suppressions_scopes" {
-  description = "List of scopes values across all monitor_alert_processing_rule_suppressions"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : v.scopes]
+  description = "Map of scopes values across all monitor_alert_processing_rule_suppressions, keyed the same as var.monitor_alert_processing_rule_suppressions"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : k => v.scopes }
 }
 output "monitor_alert_processing_rule_suppressions_tags" {
-  description = "List of tags values across all monitor_alert_processing_rule_suppressions"
-  value       = [for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : v.tags]
+  description = "Map of tags values across all monitor_alert_processing_rule_suppressions, keyed the same as var.monitor_alert_processing_rule_suppressions"
+  value       = { for k, v in azurerm_monitor_alert_processing_rule_suppression.monitor_alert_processing_rule_suppressions : k => v.tags }
 }
 
